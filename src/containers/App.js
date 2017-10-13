7bb5017af21c3ece6741c6c04260a96af96b32b1
KSJ <abbey17@naver.com>
2017-10-14 00:34:12 +0900

    액션 컴포넌트 생성

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import CounterContainer from '../containers/CounterContainer';
 
 class App extends Component {
     render() {
         return (
             <div>
                 Counter
+                <CounterContainer/>
             </div>
         );
     }
