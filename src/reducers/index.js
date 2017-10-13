5b59102c12d1d5fbb1792b886b074bfaca2c0c10
KSJ <abbey17@naver.com>
2017-10-14 00:36:47 +0900

    리듀서 함수 더하기/빼기/색상변경 구현

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -32,6 +32,44 @@ function counter(state = initialState, action) {
             return {
                 counters: counters.slice(0, counters.length - 1)
             };
+            // action.index 번째 카운터의 number 에 1 을 더합니다.
+       case types.INCREMENT:
+           return {
+               counters: [
+                   ...counters.slice(0, action.index),
+                   {
+                       ...counters[action.index],
+                       number: counters[action.index].number + 1
+                   },
+                   ...counters.slice(action.index + 1, counters.length)
+               ]
+           };
+
+       // action.index 번째 카운터의 number 에 1 을 뺍니다
+       case types.DECREMENT:
+           return {
+               counters: [
+                   ...counters.slice(0, action.index),
+                   {
+                       ...counters[action.index],
+                       number: counters[action.index].number - 1
+                   },
+                   ...counters.slice(action.index + 1, counters.length)
+               ]
+           };
+
+       // action.index 번째 카운터의 색상을 변경합니다
+       case types.SET_COLOR:
+           return {
+               counters: [
+                   ...counters.slice(0, action.index),
+                   {
+                       ...counters[action.index],
+                       color: action.color
+                   },
+                   ...counters.slice(action.index + 1, counters.length)
+               ]
+           };
 
         default:
             return state;
